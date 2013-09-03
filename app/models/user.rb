321e342010995b6722cbda024c3b03f786a22b65
Jan Milosh <janmilosh@gmail.com>
2013-09-03 14:48:08 -0400

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,13 @@ class User < ActiveRecord::Base
   									uniqueness: { case_sensitive: false}
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+  after_validation { self.errors.messages.delete(:password_digest) }
+
+  before_save :create_remember_token
+
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
+
 end
