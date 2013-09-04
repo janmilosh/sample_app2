640f2e60fde5e5e719530dcdcde620560296e5e0
Jan Milosh <janmilosh@gmail.com>
2013-09-04 08:59:54 -0400

    Remove additions that crept in and fix small bug in test.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,8 +13,10 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
 
-#	before_save { |user| user.email = email.downcase }
-	before_save { email.downcase!}
+#before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
+
+  before_save { email.downcase!}
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: 	true, 
@@ -23,15 +25,10 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
   after_validation { self.errors.messages.delete(:password_digest) }
-<<<<<<< HEAD
-=======
-
-  before_save :create_remember_token
 
   private
     def create_remember_token
       self.remember_token = SecureRandom.urlsafe_base64
     end
 
->>>>>>> sign-in-out
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -30,11 +30,7 @@ describe "User pages" do
 			it "should not create a user" do
 				expect { click_button submit }.not_to change(User, :count)
 			end
-<<<<<<< HEAD
 
-=======
-			
->>>>>>> sign-in-out
 			describe "after submission" do
 				before { click_button submit }
 
@@ -55,28 +51,15 @@ describe "User pages" do
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
 			end
-<<<<<<< HEAD
 
-=======
-			
->>>>>>> sign-in-out
 			describe "after saving the user" do
 				before { click_button submit }
 				let(:user) { User.find_by_email('user@example.com') }
 
 				it { should have_selector('title', text: user.name) }
 				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
-<<<<<<< HEAD
+				it { should have_link('Sign out') }	
 			end
-
-=======
-
-				it { should have_link('Sign out') }
-
-			end
-
-			
->>>>>>> sign-in-out
 		end
 	end
 end
